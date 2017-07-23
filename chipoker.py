277deeea5a5ae8054727f14d7b75455b85ab839f
Jon Simonsen <jonsimonsen@users.noreply.github.com>
2017-07-23 23:59:24 +0200

    Added function for sorting based on suit, then values. Made another function that counts the occurences of the different suits. Not considered a new version.

--- chipoker.py
+++ chipoker.py
@@ -1,6 +1,7 @@
 #Other libs
 
 from entities import * #Card and dealer
+from operator import attrgetter
 
 class ChiHand(object):
     """Class for a chinese poker hand."""
@@ -8,11 +9,27 @@ class ChiHand(object):
     def __init__(self, dealer):
         """Create a new random hand for chinese poker."""
 
-        self.hand = dealer.dealCards(13)
-        #self._sortHand() #Planning to sort by suits(most frequent first), then values descending.
+        self._hand = dealer.dealCards(13)
+        self._sortHand() #Planning to sort by suits(most frequent first), then values descending.
+        self._suitcounts = self._countSuits()
+
+    def _sortHand(self):
+        """Sort the hand by suits(most frequent first), then by values descending."""
+
+        self._hand.sort(key=attrgetter('_suit', '_value'), reverse=True)
+
+    def _countSuits(self):
+        """Returns a list containing the count of the various suits in the hand. The hand should be sorted in advance."""
+
+        spades = sum(card._suit == SPADES for card in self._hand)
+        hearts = sum(card._suit == HEARTS for card in self._hand)
+        diamonds = sum(card._suit == DIAMONDS for card in self._hand)
+        clubs = sum(card._suit == CLUBS for card in self._hand)
+        print(str(spades), str(hearts), str(diamonds), str(clubs))
+        return [spades, hearts, diamonds, clubs]
 
     def printHand(self):
         """Print the cards of the hand."""
 
-        for card in self.hand:
+        for card in self._hand:
             card.printCard()
